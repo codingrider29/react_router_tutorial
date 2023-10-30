107d94fdd6f08d9480e5e49fa30a692206ec324e
newtgeiszler <newtgeiszler0106@gmail.com>
2023-10-31 01:19:38 +0900

    라우터 추가, 루트 라우트 추가

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css'
+import Root from './routes/root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root/>
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>Hello world!</div>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
