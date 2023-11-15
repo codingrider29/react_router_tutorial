eebdea3ed2de3a8a6a1d6d214607783d9d3213d2
newtgeiszler <newtgeiszler0106@gmail.com>
2023-11-15 14:10:06 +0900

    데이터 수정

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 import Root, { loader as rootLoader, action as rootAction, } from './routes/root'
 import ErrorPage from './error-page'
-import Contact from './routes/contact'
+import Contact, {loader as contactLoader } from './routes/contact'
+import EditContact, {action as editAction } from './routes/editcont'
 
 const router = createBrowserRouter([
   {
@@ -17,7 +18,14 @@ const router = createBrowserRouter([
       {
         path: 'contacts/:contactId',
         element: <Contact/>,
+        loader: contactLoader,
       },
+      {
+        path: 'contacts/:contactId/edit',
+        element: <EditContact/>,
+        loader: contactLoader,
+        action: editAction,
+      }
     ],
   },
 ]);
