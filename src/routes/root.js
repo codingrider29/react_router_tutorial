228723c4f00f42df6c8dc42ff4771b400bb8d597
newtgeiszler <newtgeiszler0106@gmail.com>
2023-11-16 15:11:08 +0900

    엑티브 링크 스타일링, 로딩 UI 효과

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,4 +1,4 @@
-import { Outlet, Link, useLoaderData, Form, redirect, } from 'react-router-dom';
+import { Outlet, NavLink, useLoaderData, Form, redirect, useNavigation, } from 'react-router-dom';
 import { getContacts, createContact } from '../contacts'
 
 export async function action() {
@@ -13,6 +13,8 @@ export async function loader() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
+  
   return (
     <>
       <div id='sidebar'>
@@ -45,7 +47,10 @@ export default function Root() {
             <ul>
               {contacts.map((contact)=>(
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({isActive, isPending}) => isActive ? 'active' : isPending ? 'pending' : ''}
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -53,7 +58,7 @@ export default function Root() {
                     ) : (<i>No Name</i>
                     )}{' '}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
@@ -62,7 +67,10 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id='detail'>
+      <div
+        id='detail'
+        className={navigation.state === 'loading' ? 'loading' : ''}
+      >
         <Outlet/>
       </div>
     </>
