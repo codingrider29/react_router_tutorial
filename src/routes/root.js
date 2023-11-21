42cc18f0f91e12bdc91169fd97ac139563de81ac
newtgeiszler <newtgeiszler0106@gmail.com>
2023-11-22 00:21:50 +0900

    취소버튼 기능 구현, 검색 기능 구현

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,32 +1,40 @@
 import { Outlet, NavLink, useLoaderData, Form, redirect, useNavigation, } from 'react-router-dom';
 import { getContacts, createContact } from '../contacts'
+import { useEffect } from 'react'
 
 export async function action() {
   const contact = await createContact();
   return redirect(`/contacts/${contact.id}/edit`);
 }
 
-export async function loader() {
-  const contacts = await getContacts();
-  return { contacts };
+export async function loader( {request} ) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get('q');
+  const contacts = await getContacts(q);
+  return { contacts, q };
 }
 
 export default function Root() {
-  const { contacts } = useLoaderData();
+  const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+
+  useEffect(()=> {
+    document.getElementById('q').value = q;
+  }, [q]);
   
   return (
     <>
       <div id='sidebar'>
         <h1>React Router Contacts</h1>
         <div>
-          <form id='search-form' role='search'>
+          <Form id='search-form' role='search'>
             <input
               id='q'
               aria-label='Search contacts'
               placeholder='Search'
               type='search'
               name='q'
+              defaultValue={q}
             />
             <div
               id='search-spinner'
@@ -37,7 +45,7 @@ export default function Root() {
               className='sr-only'
               aria-live='polite'
             ></div>
-          </form>
+          </Form>
           <Form method='post'>
             <button type='submit'>New</button>
           </Form>
