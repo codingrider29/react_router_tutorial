ee74573026f5a16cef592ebd351834d676813ddb
evh0106 <evh0106@naver.com>
2023-12-08 12:31:52 +0900

    탐색 없는 변형, 낙관적UI 추가

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 import Root, { loader as rootLoader, action as rootAction, } from './routes/root'
 import ErrorPage from './error-page'
-import Contact, {loader as contactLoader } from './routes/contact'
+import Contact, {loader as contactLoader, action as contactAction } from './routes/contact'
 import EditContact, {action as editAction } from './routes/editcont'
 import { action as destroyAction } from './routes/destroy'
 import Index from './routes/index'
@@ -22,6 +22,7 @@ const router = createBrowserRouter([
         path: 'contacts/:contactId',
         element: <Contact/>,
         loader: contactLoader,
+        action: contactAction,
       },
       {
         path: 'contacts/:contactId/edit',
--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,14 +1,20 @@
-import { Form, useLoaderData } from 'react-router-dom';
-import { getContact } from '../contacts'
+import { Form, useLoaderData, useFetcher, } from 'react-router-dom';
+import { getContact, updateContact } from '../contacts'
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
   return { contact };
 }
 
+export async function action({ request, params }) {
+  let formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get('favorite') === 'true',
+  });
+}
+
 export default function Contact() {
   const { contact } = useLoaderData();
-
   return (
     <div id='contact'>
       <div>
@@ -69,10 +75,13 @@ export default function Contact() {
 }
 
 function Favorite({ contact }) {
-  // yes, this is a `let` for later
+  const fetcher = useFetcher();
   let favorite = contact.favorite;
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get('favorite') === 'true';
+  }
   return (
-    <Form method='post'>
+    <fetcher.Form method='post'>
       <button
         name='favorite'
         value={favorite ? 'false' : 'true'}
@@ -84,6 +93,6 @@ function Favorite({ contact }) {
       >
         {favorite ? '★' : '☆'}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
