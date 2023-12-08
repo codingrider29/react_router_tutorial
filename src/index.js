79f39b97eb04db94397bcde688048fa4a30ead98
evh0106 <codingrider29@gmail.com>
2023-12-08 15:59:24 +0900

    JSX 스타일 라우트

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
 import './index.css'
 import Root, { loader as rootLoader, action as rootAction, } from './routes/root'
 import ErrorPage from './error-page'
@@ -9,39 +9,37 @@ import EditContact, {action as editAction } from './routes/editcont'
 import { action as destroyAction } from './routes/destroy'
 import Index from './routes/index'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root/>,
-    errorElement: <ErrorPage/>,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      {
-        errorElement: <ErrorPage/>,
-        children: [
-          { index: true, element: <Index/> },
-          {
-            path: 'contacts/:contactId',
-            element: <Contact/>,
-            loader: contactLoader,
-            action: contactAction,
-          },
-          {
-            path: 'contacts/:contactId/edit',
-            element: <EditContact/>,
-            loader: contactLoader,
-            action: editAction,
-          },
-          {
-            path: 'contacts/:contactId/destroy',
-            action: destroyAction,
-          }
-        ]
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path = '/'
+      element={<Root/>}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage/>}
+    >
+      <Route errorElement={<ErrorPage/>}>
+        <Route index element={<Index/>}/>
+        <Route
+          path='contacts/:contactId'
+          element={<Contact/>}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path='contacts/:contactId/edit'
+          element={<EditContact/>}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route
+          path='contacts/:contactId/destroy'
+          action={destroyAction}
+        />
+      </Route>
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
